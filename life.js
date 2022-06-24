c9e94ce9d43dcd4367ae30e25da8f161213113e5
Preciousokeke25 <preciousokeke393@gmail.com>
2022-06-24 21:23:41 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,29 @@
+
+function calculate() {
+    var a = parseInt(prompt("Enter first number"));
+    var b = parseInt(prompt("Enter second number"));
+    var operator = prompt("Enter operator from the list below: +, -, *, /, %");
+    var result;
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        case "%":
+            result = modulus(a, b);
+            break;
+        default:
+            alert("Invalid operator");
+            break;
+    }
+    alert(result);
+}
+calculate();
